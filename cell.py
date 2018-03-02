8bccaa6467bed87a0f19137e7d493326779f325f
Tim Williamson <32397024+Le5tes@users.noreply.github.com>
2018-03-02 17:12:27 +0000

    Adds rules for living cells
    stay alive if 2/3 living neighbours, else dies

--- cell.py
+++ cell.py
@@ -6,8 +6,12 @@ class Cell:
 	def prep(self):
 		surrounders_alive = [cell.is_alive for cell in self.__surrounders].count(True)
 
-		if self.is_alive == False and  surrounders_alive == 3:
+		if self.is_alive == False and surrounders_alive == 3:
 			self.__next = True
+		elif self.is_alive and (surrounders_alive == 2 or surrounders_alive == 3):
+			self.__next = True
+		else:
+			self.__next = False
 
 	def update(self):
 		self.is_alive = self.__next
--- cell_test.py
+++ cell_test.py
@@ -18,3 +18,24 @@ def test_becomes_alive_if_3_living_cells_nearby():
 	cell.update()
 	assert cell.is_alive == True
 
+def test_stays_alive_with_2_or_3_living_cells_nearby():
+	cell = setup_cell(True,2)
+	cell.prep()
+	cell.update()
+	assert cell.is_alive == True
+	cell = setup_cell(True,3)
+	cell.prep()
+	cell.update()
+	assert cell.is_alive == True
+
+def test_dies_with_fewer_living_cells_nearby():
+	cell = setup_cell(True,1)
+	cell.prep()
+	cell.update()
+	assert cell.is_alive == False
+
+def test_dies_with_too_many_living_cells_nearby():
+	cell = setup_cell(True,4)
+	cell.prep()
+	cell.update()
+	assert cell.is_alive == False
