24c49a361811f6aecf2012c61634e980118bd285
Tim Williamson <32397024+Le5tes@users.noreply.github.com>
2018-03-02 16:59:41 +0000

    Cells come alive if 2 or 3 neighbours are alive

--- cell.py
+++ cell.py
@@ -2,3 +2,12 @@ class Cell:
 	is_alive = False
 	def __init__(self,surrounders):
 		self.__surrounders = surrounders
+
+	def prep(self):
+		surrounders_alive = [cell.is_alive for cell in self.__surrounders].count(True)
+
+		if self.is_alive == False and (surrounders_alive == 2 or surrounders_alive == 3):
+			self.__next = True
+
+	def update(self):
+		self.is_alive = self.__next
--- cell_test.py
+++ cell_test.py
@@ -1,5 +1,20 @@
 from cell import Cell
 
+def setup_cell(is_alive,num_of_surrounding_alive):
+	living_cell = Cell([])
+	living_cell.is_alive = True
+	surrounders= [living_cell] * num_of_surrounding_alive
+	cell = Cell(surrounders)
+	cell.is_alive = is_alive
+	return cell 
+
 def test_starts_dead():
 	cell = Cell([])
 	assert cell.is_alive == False
+
+def test_becomes_alive_if_2_living_cells_nearby():
+	cell = setup_cell(False,2)
+	cell.prep()
+	cell.update()
+	assert cell.is_alive == True
+
