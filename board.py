18e1588d474320d2b82cce6e3c925b6a4a2e6156
Tim Williamson <32397024+Le5tes@users.noreply.github.com>
2018-03-05 17:03:03 +0000

    board now provides a cell with its neighbours

--- board.py
+++ board.py
@@ -2,11 +2,22 @@ from cell import Cell
 
 class Board:
 
-	def __init__(self,size):
-		self.cells = [Cell([]) for _ in range(size)]
+	def __init__(self,size, cell_class = Cell):
+		self.cells = [[cell_class() for _ in range(size)] for _ in range(size)]
+		for x in range(size):
+			for y in range(size):
+				print(str(self.cells[x][y]))
+				self.cells[x][y].set_surrounders(self.__get_surrounders(x,y,size))
 
 	def update(self):
-		for cell in self.cells:
-			cell.prep()
-		for cell in self.cells:
-			cell.update()
+		for row_of_cells in self.cells:
+			for cell in row_of_cells:
+				cell.prep()
+			for cell in row_of_cells:
+				cell.update()
+
+	def __get_surrounders(self,x,y,size):
+		return [self.cells[p][q] 
+				for p in range(x-1, x+2)
+				for q in range(y-1, y+2)
+				if p >= 0 and q >= 0 and p < size and q < size and (p != x or q!= y)]
--- board_test.py
+++ board_test.py
@@ -1,17 +1,30 @@
 from board import Board
+from cell import Cell
 import pytest
 
 @pytest.fixture
 def subject():
 	return Board(5)
 
+@pytest.fixture
+def mockcell(mocker):
+	return mocker.Mock(spec=Cell)
+
+
 def test_update_preps_cells(subject, mocker):
-	mocker.spy(subject.cells[0],'prep')
+	mocker.spy(subject.cells[0][0],'prep')
 	subject.update()
-	assert subject.cells[0].prep.call_count == 1
+	assert subject.cells[0][0].prep.call_count == 1
 
 def test_update_updates_cells(subject, mocker):
-	mocker.spy(subject.cells[0],'update')
+	mocker.spy(subject.cells[0][0],'update')
 	subject.update()
-	assert subject.cells[0].update.call_count == 1
+	assert subject.cells[0][0].update.call_count == 1
 
+def test_initialises_cells_with_their_neighbours(mockcell):
+	def cell_double():
+		return mockcell
+
+	Board(5,cell_double)
+	mockcell.set_surrounders.assert_called_with([mockcell,mockcell,mockcell])
+	
