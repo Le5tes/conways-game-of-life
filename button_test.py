3a81f7f8c0f780856c220949c7fd9220969494dc
Tim Williamson <32397024+Le5tes@users.noreply.github.com>
2018-03-08 20:57:06 +0000

    adds test of the interaction of a button with a class

--- button_test.py
+++ button_test.py
@@ -23,3 +23,21 @@ def test_if_not_within_bounds_do_nothing(subject):
 	subject.click(XY(16,16))
 
 	subject.function.assert_not_called()
+
+
+class MyObj:
+	def __init__(self):
+		self.value = 0
+		self.button = Button(XY(5,5),XY(10,10), 'Button', self.method)
+
+	def click(self, point):
+		self.button.click(point)
+
+	def method(self):
+		self.value +=1
+
+
+def test_interaction():
+	foo = MyObj()
+	foo.click(XY(10,10))
+	assert foo.value == 1
